2c64dcefce561a6b272ce72a95ede1461d9d15fd
Lev <w9430@yandex.ru>
2024-04-13 01:13:01 +0300

    Base project was created, storyboard references were removed

---
+++ SimpleFoodDeliveryApp/SimpleFoodDeliveryApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SimpleFoodDeliveryApp
+//
+//  Created by Лев Куликов on 13.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        view.backgroundColor = .green
+    }
+
+
+}
+
