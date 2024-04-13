190c2bd5bd3ddd3da5a6febb82d4c68047be805a
Lev <w9430@yandex.ru>
2024-04-13 16:28:17 +0300

    Added new fonts

--- SimpleFoodDeliveryApp/SimpleFoodDeliveryApp/ViewController.swift
+++ SimpleFoodDeliveryApp/SimpleFoodDeliveryApp/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
             label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
         ])
         
-        label.font = .systemFont(ofSize: 40, weight: UIFont.Weight(40))
+        label.font = .Roboto.thin.size(of: 40)
         label.textColor = .white
         
         view.backgroundColor = .red
